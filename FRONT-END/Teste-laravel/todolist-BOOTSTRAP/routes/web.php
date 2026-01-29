d8a435c1f4722d643dae0a515c4ac17803997ce1
Nicolas Gabriel Da Silva Bentes <nicolasg.s.bentes@gmail.com>
2026-01-29 18:54:35 -0300

    Troca de localização teste

--- FRONT-END/Teste-laravel/todolist-BOOTSTRAP/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TaskController;
-
-Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
-Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
-Route::put('/tasks/{id}', [TaskController::class, 'update'])->name('tasks.update');
-Route::delete('/tasks/{id}', [TaskController::class, 'destroy'])->name('tasks.destroy');
